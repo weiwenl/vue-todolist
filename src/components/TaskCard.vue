12e4ed0f35402cfbd4fdd7bb210f6329cc04585f
Justin <weiwenl.justin@gmail.com>
2021-11-19 16:56:54 +0800

    Create task card component

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-card>
+      <b-row>
+        <b-col> Completed Button </b-col>
+        <b-col>
+          <h4>{{ task.title }}</h4>
+          <h6 class="text-secondary">{{ task.description }}</h6>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-card-text align="right">{{ task.createdDate }}</b-card-text>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style></style>
