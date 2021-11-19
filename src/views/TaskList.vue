2c58d4e80e311cf36e7451ec2688086d3dcb4b3b
Justin <weiwenl.justin@gmail.com>
2021-11-19 16:57:59 +0800

    Add task card component to task list, and get tasks from local api

--- src/views/TaskList.vue
+++ src/views/TaskList.vue
@@ -2,11 +2,34 @@
   <div>
     <h2>My Tasks</h2>
     <h4>Let's be productive today.</h4>
+    <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
   </div>
 </template>
 
 <script>
-export default {};
+import TaskCard from "@/components/TaskCard.vue";
+import axios from "axios";
+export default {
+  components: {
+    TaskCard,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/tasks")
+      .then((response) => {
+        console.log(response);
+        this.tasks = response.data;
+      })
+      .catch((error) => {
+        console.log("There was an error:", error.response);
+      });
+  },
+};
 </script>
 
 <style></style>
